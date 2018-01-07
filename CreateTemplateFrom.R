4d8885e542221d2dc928f4511efe3d19c617be35
Hatem Hosny <hatemhosny@gmail.com>
2018-01-07 20:00:15 +0200

    createtemplate using R instead of powershell
    powershell caused corruption of the template file

--- CreateTemplateFrom.R
+++ CreateTemplateFrom.R
@@ -1,25 +1,25 @@
 CreateTemplateFrom <- function(source, output="template.xlsx", sheet.to.keep="Charts",
                                PSscript="CreateTemplate.ps1") {
 
-  system2("powershell", args=c("-file", PSscript, source, output, sheet.to.keep))
+  # system2("powershell", args=c("-file", PSscript, source, output, sheet.to.keep))
 
-  # if (!file.exists(source)) print(paste("The source file:", source, "does not exist"))
-  # stopifnot(file.exists(source))
-  #
-  # copyAndLoadWorkbook <- function(source.file, output.file) {
-  #     file.copy(source.file, output.file, overwrite=TRUE)
-  #     wb <- loadWorkbook(output.file)
-  #   wb
-  # }
-  #
-  # wb <- copyAndLoadWorkbook(source, output)
-  # sheets <- getSheets(wb)
-  # sheets.to.remove <- setdiff(names(sheets), sheet.to.keep)
-  #
-  # for (sheetName in sheets.to.remove) {
-  #   removeSheet(wb, sheetName = sheetName)
-  # }
-  #
-  # saveWorkbook(wb, output)
+  if (!file.exists(source)) print(paste("The source file:", source, "does not exist"))
+  stopifnot(file.exists(source))
+
+  copyAndLoadWorkbook <- function(source.file, output.file) {
+      file.copy(source.file, output.file, overwrite=TRUE)
+      wb <- loadWorkbook(output.file)
+    wb
+  }
+
+  wb <- copyAndLoadWorkbook(source, output)
+  sheets <- getSheets(wb)
+  sheets.to.remove <- setdiff(names(sheets), sheet.to.keep)
+
+  for (sheetName in sheets.to.remove) {
+    removeSheet(wb, sheetName = sheetName)
+  }
+
+  saveWorkbook(wb, output)
 
 }
